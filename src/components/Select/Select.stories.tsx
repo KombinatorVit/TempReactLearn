25cfd5a833496d7812ad2699021839ba11793730
Vit Onischenko <slonik0775@gmail.com>
2022-09-06 00:54:41 +0300

    15 - React кабзда как подробно: Select component Жесткое видео))

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,27 +1,48 @@
-
 import {useState} from 'react';
+import {Select} from './Select';
+import {action} from '@storybook/addon-actions';
 
 
 export default {
-    title: 'input',
-    // component: input,
+    title: 'Select',
+    component: Select,
 };
 
 
+export const WithValue = () => {
+    const [value, setValue] = useState('2');
+
+    return (
+        <div>
+            <Select onChange={setValue}
+                    value={value}
+                    items={[
+                        {value: '1', title: 'KKKK'},
+                        {value: '2', title: 'ddddd'},
+                        {value: '3', title: 'mmm'}
+                    ]}/>
 
 
+        </div>
+    );
+};
 
-export const ControlledSelect = () => {
-    const [parentValue, setParentValue] = useState<string | undefined>('2')
-    return <select value={parentValue} onChange={(e)=> {
-        setParentValue(e.currentTarget.value)
-    }
-    }>
 
-        <option>none</option>
-        <option value={'1'}>Minsk</option>
-        <option value={'2'}>Dnipro</option>
-        <option value={'3'}>Kiev</option>
-    </select>
-}
+export const WithoutValue = () => {
+    const [value, setValue] = useState(null);
 
+    return (
+        <div>
+            <Select
+                onChange={setValue}
+                value={value}
+                items={[
+                    {value: '1', title: 'KKKK'},
+                    {value: '2', title: 'ddddd'},
+                    {value: '3', title: 'dddddd'}
+                ]}/>
+
+
+        </div>
+    );
+};
