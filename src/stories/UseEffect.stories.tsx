e70d6ffa354b43c80c219d68335126945651171c
Vit Onischenko <slonik0775@gmail.com>
2022-10-25 15:17:10 +0300

    21 - React кабзда как подробно: useEffect основы

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -12,8 +12,20 @@ export const SimpleExample = () => {
 
 
     useEffect(()=> {
-        console.log('useEffect');
+        console.log('useEffect every render');
+        document.title = counter.toString()
     })
+
+
+    useEffect(()=> {
+        console.log('useEffect only first render(componentDidMount');
+        document.title = counter.toString()
+    }, [])
+
+    useEffect(()=> {
+        console.log('useEffect first render and every counter change');
+        document.title = counter.toString()
+    },[counter])
 
 return <>
 Hello, {counter}
